d99330176027a6071a7ebf9246db9c0e36dbda72
Artem <artemsihalev560@gmail.com>
2026-03-30 18:38:00 +0300

    Update ping_pong.py
    update Player class

--- ping_pong.py
+++ ping_pong.py
@@ -26,12 +26,20 @@ class Player(GameSprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
         super().__init__(player_image, player_x, player_y, size_x, size_y, player_speed)
 
-    def update(self):
+    def update_l(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
-            self.rect.x -= self.speed
+            self.rect.y -= self.speed
         if keys[K_s] and self.rect.y < 495:
-            self.rect.x += self.speed
+            self.rect.y += self.speed
+    
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 495:
+            self.rect.y += self.speed
+
 
 while game:
     for e in event.get():
